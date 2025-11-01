9f9bd801205ff8cbb53f39331cc741709f68cb2e
PDDigfhufkrb <eksort.aurel@gmail.com>
2025-11-02 02:32:30 +0300

    Delete api/payment/create.js

--- api/payment/create.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-  console.log('✅ API работает!');
-  
-  res.status(200).json({ 
-    success: true, 
-    payment_url: 'https://yookassa.ru/'
-  });
-}
